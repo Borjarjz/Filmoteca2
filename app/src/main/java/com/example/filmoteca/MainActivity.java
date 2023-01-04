f671a331132ba2f4b9062018e890e39f573c300d
Borja Rodriguez <brodrjim@myuax.com>
2023-01-04 20:06:19 +0100

    Se crea las dos actionbar para todos los activities y el inflater y el CASE en la clase principal-.
    funcionando toast de los case

--- app/src/main/java/com/example/filmoteca/MainActivity.java
+++ app/src/main/java/com/example/filmoteca/MainActivity.java
@@ -1,8 +1,12 @@
 package com.example.filmoteca;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,33 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {//Se crea el menú que queramos para nuestra activity (inflate)
+        getMenuInflater().inflate(R.menu.menu1,menu);
+        return true;
+       // return super.onCreateOptionsMenu(menu);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {//se hace el CASE para cada opción del menu
+        switch(item.getItemId()){
+            case R.id.idioma:
+                Toast.makeText(MainActivity.this,"ha pulsado idioma",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.editpeli:
+                Toast.makeText(MainActivity.this,"ha pulsado editar",Toast.LENGTH_SHORT).show();
+                return true;
+
+            case R.id.borrarpeli:
+                Toast.makeText(MainActivity.this,"ha pulsado borrar",Toast.LENGTH_SHORT).show();
+                return true;
+
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+
     }
 }
