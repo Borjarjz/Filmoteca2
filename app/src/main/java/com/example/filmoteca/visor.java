be7f3ecac5a02207faabaf918b2f7616756c1811
Borja Rodriguez <brodrjim@myuax.com>
2023-01-06 17:22:35 +0100

    Ya se puede consultar la info de la BBDD en el visor!!
    falta hacer el editor.

--- app/src/main/java/com/example/filmoteca/visor.java
+++ app/src/main/java/com/example/filmoteca/visor.java
@@ -8,10 +8,24 @@ import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ListView;
+import android.widget.TextView;
 import android.widget.Toast;
 
-public class visor extends AppCompatActivity {
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.Query;
+import com.google.firebase.database.ValueEventListener;
 
+public class visor extends AppCompatActivity {
+    FirebaseDatabase database = FirebaseDatabase.getInstance();
+    DatabaseReference raiz = database.getReference("PELICULAS");
+    String pelipulsada=null;
 
 
     @Override
@@ -19,8 +33,59 @@ public class visor extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_visor);
         Intent intent = getIntent();
-        String pelipulsada = intent.getStringExtra("MESSAGE");
+        pelipulsada = intent.getStringExtra("MESSAGE");
         Toast.makeText(visor.this,"se pasa "+pelipulsada,Toast.LENGTH_SHORT).show();
+
+
+
+
+        Query query =raiz.orderByChild("Titulo").equalTo(pelipulsada);
+
+
+        setContentView(R.layout.activity_visor);
+
+        TextView tit=(TextView)findViewById(R.id.titulomostrar);
+        TextView gen=(TextView)findViewById(R.id.generomostrar);
+        TextView sin=(TextView)findViewById(R.id.sinopsismostrar);
+        TextView dir=(TextView)findViewById(R.id.directormostrar);
+        TextView vis=(TextView)findViewById(R.id.vistamostrar);
+
+
+        query.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                // This method is called once with the initial value and again
+                // whenever data at this location is updated.
+                //String value = snapshot.getValue(String.class);
+
+
+
+                for (DataSnapshot snapshot : dataSnapshot.getChildren()) {
+
+                    tit.setText(snapshot.child("Titulo").getValue(String.class));
+                    gen.setText(snapshot.child("Genero").getValue(String.class));
+                    sin.setText(snapshot.child("Sinopsis").getValue(String.class));
+                    dir.setText(snapshot.child("Director").getValue(String.class));
+                    vis.setText(snapshot.child("vista").getValue(Boolean.class).toString());
+
+                }
+
+
+
+
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+                // Failed to read value
+
+            }
+        });
+
+
+
+
+
     }
 
 
@@ -66,8 +131,9 @@ public class visor extends AppCompatActivity {
 
 
     public void irActivityeditor() {//método que lanzara la actividad siguiente con un intent
-        Intent intent = new Intent(visor.this,editor.class);
-        startActivity(intent);
+        Intent intent2 = new Intent(visor.this,editor.class);
+        intent2.putExtra("MESSAGE", pelipulsada);
+        startActivity(intent2);
 
     }
 }
